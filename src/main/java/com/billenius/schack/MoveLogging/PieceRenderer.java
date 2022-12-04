f52f3e7d0dcec3180d7e6ff155da360aa1bb0ec5
Love Billenius <lovebillenius@disroot.org>
2022-12-04 18:14:08 +0100

    move logging logic

---
+++ src/main/java/com/billenius/schack/MoveLogging/PieceRenderer.java
@@ -0,0 +1,25 @@
+package com.billenius.schack.MoveLogging;
+
+import java.awt.Component;
+import java.awt.image.BufferedImage;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+public class PieceRenderer extends JLabel implements ListCellRenderer<Move> {
+
+    @Override
+    public Component getListCellRendererComponent(JList<? extends Move> list, Move move, int index,
+            boolean isSelected,
+            boolean cellHasFocus) {
+        BufferedImage image = move.movedPiece.getIcon();
+        ImageIcon ii = new ImageIcon(image.getScaledInstance(40, 40, java.awt.Image.SCALE_SMOOTH));
+
+        setIcon(ii);
+        setText(move.toString());
+
+        return this;
+    }
+}
